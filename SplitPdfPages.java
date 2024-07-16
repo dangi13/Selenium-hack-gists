942bf8977b79d30791204bcd7e287d3d5db98bea
dangi13 <40757536+dangi13@users.noreply.github.com>
2024-07-16 23:13:04 +0530

    PDF File splitter

---
+++ SplitPdfPages.java
@@ -0,0 +1,28 @@
+package com.demo.core;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+
+public class SplitPdfPages {
+	public static void main(String[] args) throws DocumentException, IOException {
+		String pdfFileLocation = "/Users/jaikantdangi/Downloads/demo/demo.pdf";
+	    PdfReader reader = new PdfReader(pdfFileLocation);
+	    int n = reader.getNumberOfPages();
+	    reader.close();
+	    String path;
+	    PdfStamper stamper;
+	    for (int i = 1; i <= n; i++) {
+	        reader = new PdfReader(pdfFileLocation);
+	        reader.selectPages(String.valueOf(i));
+	        path = String.format("p-%s.pdf", i);
+	        stamper = new PdfStamper(reader,new FileOutputStream(path));
+	        stamper.close();
+	        reader.close();
+	    }
+	}
+}
+
